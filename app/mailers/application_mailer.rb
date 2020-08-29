79bacf0531439ecc2cab511856b8dd63806edaba
Manavalan <manavalan@10decoders.in>
2020-08-29 18:47:32 +0530

    send grid

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV['MAILER_FROM_ADDRESS'] || 'no-reply@getvista.co'
+  default from: ENV['MAILER_FROM_ADDRESS'] || 'manavalan@10decoders.in'
   layout 'mailer'
 end
